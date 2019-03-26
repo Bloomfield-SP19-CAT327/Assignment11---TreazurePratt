73af6159a51ce6832cb0c1f4f17da9b6991da549
Treazure Pratt <36020362+TreazurePratt@users.noreply.github.com>
2019-03-26 13:38:40 -0400

    Add files via upload

---
+++ BasicNetworking/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    GameObject playerToSpawn;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        playerToSpawn.GetComponent<Player>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+        playerToSpawn.GetComponent<Renderer>().material.color = playerToSpawn.GetComponent<Player>().color;
+        NetworkServer.AddPlayerForConnection(conn, playerToSpawn, playerControllerId);
+    }
+}
---
+++ BasicNetworking/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    [SyncVar]
+    public Color color;
+
+    float moveSpeed = 1.875f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void GetInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        MoveIt(x, y);
+    }
+    void MoveIt(float x, float y)
+    {
+        transform.Translate(x, y, 0);
+    }
+}
